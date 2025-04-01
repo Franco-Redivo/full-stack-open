60e60cfcecf23c68446f7693f5ca1d08bbc25544
Franco-Redivo <franco161101@gmail.com>
2025-04-01 18:15:16 -0300

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '74721862' }
   ]) 
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
-  const handleInputChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
   }
 
   const handleAddPerson = (e) => {
     e.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newPhone
     }
-    const array = persons.filter(person => person.name === newName);
-    (array.length > 0) ? alert(`${newName} is already added to phonebook`)  : setPersons(persons.concat(newPerson));  
-    setNewName('');
+    if(newName === '' || newPhone === ''){
+      alert("missing value");
+    }else{
+      const array = persons.filter(person => person.name === newName);
+      (array.length > 0) ? alert(`${newName} is already added to phonebook`)  : setPersons(persons.concat(newPerson));  
+      setNewName('');
+      setNewPhone('');
+    } 
+  }
+
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
   }
 
   return (
@@ -25,14 +36,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleInputChange} value={newName}/>
+          name: <input type='text' onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          number: <input type='text' onChange={handlePhoneChange} value={newPhone}></input>
         </div>
         <div>
           <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
