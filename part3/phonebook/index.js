a76e209d66129b9a3e8f55f6c520fd7f009e5403
Franco-Redivo <franco161101@gmail.com>
2025-04-11 23:39:27 -0300

    exercise 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 app.use(express.static('dist'));
-app.use(cors());
 app.use(express.json());
 app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms :post')
