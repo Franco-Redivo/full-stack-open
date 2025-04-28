c7d10ee60b91ac59f49976c98251d34141d2ca55
Franco-Redivo <franco161101@gmail.com>
2025-04-28 20:13:09 -0300

    exercise 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,46 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total Likes', () => {
+    const listWithOneBlog = [
+        {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            id: '5a422aa71b54a676234d17f8',
+        }
+    ]
+    const listWithTwoBlogs = [
+        {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            id: '5a422aa71b54a676234d17f8',
+        },
+        {
+            title: 'Go To Statement Considered Harmful Pt 2',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 32,
+            id: '6b642a71b54a676234d17f8',
+        }
+
+    ]
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+    test('of empty list is zero',() => {
+        const result = listHelper.totalLikes([])
+        assert.strictEqual(result, 0)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        assert.strictEqual(result,37)
+    })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1;
 }
 
-module.exports = {
-    dummy
+const totalLikes = (blogList) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+    return blogList.length === 0
+    ? 0
+    : blogList.reduce(reducer, 0);
 }
+
+module.exports = {dummy,totalLikes}
