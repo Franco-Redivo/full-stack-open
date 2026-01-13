e8b4eb2c945530df7b9c9f8994399566da89582a
Franco Redivo <franco161101@gmail.com>
2026-01-13 16:08:52 -0300

    Add bookCount field to Author type and implement allAuthors query

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -93,6 +93,7 @@ const typeDefs = `
     name: String!
     born: Int!
     id: ID!
+    bookCount: Int!
   }
 
   type Book {
@@ -106,7 +107,8 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -114,7 +116,15 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books
+    allBooks: (root, args) => {
+        if(!args.author) return books
+        return books.filter((book) => book.author === args.author)
+    },
+    allAuthors: () => authors
+
+  },
+  Author: {
+    bookCount: (root) => books.filter((book) => book.author == root.name).length
   }
 }
 
