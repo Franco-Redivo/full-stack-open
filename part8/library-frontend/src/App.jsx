f1b3c5b588e5d63262a237be2e6275015b01a6ad
Franco Redivo <franco161101@gmail.com>
2026-01-19 17:54:55 -0300

    feat: add books query and update Books component to display book list

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -14,15 +14,27 @@ const ALL_AUTHORS = gql `
     }
   }
 `
+const ALL_BOOKS = gql `
+  query {
+    allBooks {
+      id
+      title
+      author
+      published
+    }
+  }
+`
 
 const App = () => {
   const [page, setPage] = useState('authors')
 
-  const result = useQuery(ALL_AUTHORS)
+  const authorsQueryResult = useQuery(ALL_AUTHORS)
+  const booksQueryResult = useQuery(ALL_BOOKS)
 
-  if(result.loading){
+  if(authorsQueryResult.loading || booksQueryResult.loading){
     return <div>Loading...</div>
   }
+
 
   return (
     <div>
@@ -32,9 +44,9 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
+      <Authors show={page === 'authors'} authors={authorsQueryResult.data?.allAuthors || []} />
 
-      <Books show={page === 'books'} />
+      <Books show={page === 'books'} books={booksQueryResult.data?.allBooks || []} />
 
       <NewBook show={page === 'add'} />
     </div>
