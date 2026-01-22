1a494d57146549f698c6a03c9e7d7e48b977a2c2
Franco Redivo <franco161101@gmail.com>
2026-01-22 18:19:09 -0300

    feat: add mongoose models for Author and Book

---
+++ part8/library-backend/models/author.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    born: {
+        type: Number,
+        required: false,
+    },
+    
+})
+
+module.exports = mongoose.model('Author', schema)
---
+++ part8/library-backend/models/book.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    published: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    genres: {
+        type: [String],
+        required: true,
+    }
+})
+
+module,exports = mongoose.model('Book', schema)
