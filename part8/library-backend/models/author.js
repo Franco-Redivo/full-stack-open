e2300b670afa4d9446286fa0410cdc53696b6a79
Franco Redivo <franco161101@gmail.com>
2026-01-23 17:54:24 -0300

    fix: update Author and Book models to correct field requirements and types

--- part8/library-backend/models/author.js
+++ part8/library-backend/models/author.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5
-    },
-    born: {
-        type: Number,
-        required: false,
-    },
-    
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+  },
+  born: {
+    type: Number,
+  },
 })
 
 module.exports = mongoose.model('Author', schema)
--- part8/library-backend/models/book.js
+++ part8/library-backend/models/book.js
@@ -1,24 +1,20 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minlength: 5
-    },
-    published: {
-        type: Number,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true,
-        minlength: 5
-    },
-    genres: {
-        type: [String],
-        required: true,
-    }
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author',
+  },
+  genres: [{ type: String }],
 })
 
-module,exports = mongoose.model('Book', schema)
+module.exports = mongoose.model('Book', schema)
