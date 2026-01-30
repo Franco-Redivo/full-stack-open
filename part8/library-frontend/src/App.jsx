24d67e6f3b9e454bb31ba02aa1994193711afb82
Franco-Redivo <franco161101@gmail.com>
2026-01-30 19:49:11 -0300

    feat: enhance App component with error notification and logout functionality

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-import Login from './components/Login'
+import Notify from './components/Notify'
+import LoginForm from './components/LoginForm'
 import { gql } from '@apollo/client'
-import { useQuery } from '@apollo/client/react'
+import { useQuery, useApolloClient } from '@apollo/client/react'
 
 const ALL_AUTHORS = gql `
   query {
@@ -33,13 +34,39 @@ const ALL_BOOKS = gql `
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(localStorage.getItem('user-token'))
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const authorsQueryResult = useQuery(ALL_AUTHORS)
   const booksQueryResult = useQuery(ALL_BOOKS)
+  const client = useApolloClient
 
   if(authorsQueryResult.loading || booksQueryResult.loading){
     return <div>Loading...</div>
   }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const onLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+    setPage('authors')
+  }
+
+  // if(!token) {
+  //   return (
+  //     <div>
+  //       <Notify errorMessage={errorMessage}/>
+  //       <h2>Login</h2>
+  //       <LoginForm setToken={setToken} setError={notify}/>
+  //     </div>
+  //   )
+  // }
 
 
   return (
@@ -47,17 +74,26 @@ const App = () => {
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-        <button onClick={() => setPage('login')} >Login</button>
+        {!token ? 
+          <button onClick={() => setPage('login')} >Login</button>
+          :
+          <div>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={onLogout} >Logout</button>
+          </div>
+          
+          
+        }
+        
       </div>
 
-      <Authors show={page === 'authors'} authors={authorsQueryResult.data?.allAuthors || []} />
+      <Authors show={page === 'authors'} token={token} setError={notify} authors={authorsQueryResult.data?.allAuthors || []} />
 
       <Books show={page === 'books'} books={booksQueryResult.data?.allBooks || []} />
 
-      <NewBook show={page === 'add'} />
+      <NewBook show={page === 'add'} setError={notify} />
 
-      <Login show={page === 'login'} setToken={setToken}/>
+      <LoginForm show={page === 'login'} setError={notify} setToken={setToken} setPage={setPage}/>
     </div>
     
   )
