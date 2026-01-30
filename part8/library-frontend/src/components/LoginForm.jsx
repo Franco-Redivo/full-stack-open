35ae8b13a8ba43c9bea0e251f01d6ce2f1256d4d
Franco-Redivo <franco161101@gmail.com>
2026-01-30 19:49:54 -0300

    feat: add LoginForm component for user authentication

---
+++ part8/library-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import { useMutation } from "@apollo/client/react";
+import { gql } from "@apollo/client";
+
+const LOGIN = gql `
+    mutation login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            value
+        }
+    }
+`
+
+const LoginForm = ({ setError, setToken, show ,setPage}) => {
+    const[username, setUsername] = useState('')
+    const[password, setPassword] = useState('')
+
+    const [login] = useMutation(LOGIN, {
+        onCompleted: (data) => {
+            const token = data.login.value
+            setToken(token)
+            localStorage.setItem('user-token', token)
+            setPage('authors')
+        },
+        onError: (error) => {
+            setError(error.message)
+        }
+    })
+
+    const submit = (event) => {
+        event.preventDefault()
+        login({ variables: {username, password } })
+    }
+
+    if(!show){
+        return null
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <div>
+                username
+                <input
+                    value={username}
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password
+                <input
+                    value={password}
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>
+    )
+}
+
+export default LoginForm
