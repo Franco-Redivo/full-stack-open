0809128a318eb00e9d3e3ed74424e9acc5543c48
Franco-Redivo <franco161101@gmail.com>
2026-01-31 19:56:31 -0300

    feat: implement genre filtering and add genre buttons in Books component

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,4 +1,18 @@
+import { gql } from "@apollo/client"
+import { useState } from "react"
+import { useQuery } from "@apollo/client/react"
+
+const ALL_GENRES = gql `
+  query {
+    allGenres
+  }
+`
+
 const Books = ( {show, books } ) => {
+  const [genre, setGenre] = useState('all genres')
+
+  const genresResult = useQuery(ALL_GENRES)
+
   if (!show) {
     return null
   }
@@ -16,14 +30,22 @@ const Books = ( {show, books } ) => {
             <th>published</th>
           </tr>
           {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author?.name}</td>
-              <td>{a.published}</td>
-            </tr>
+            genre === 'all genres' || a.genres.includes(genre) ? (
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author?.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ) : null
           ))}
         </tbody>
       </table>
+      <div>
+        {genresResult.data.allGenres.map((g) => (
+          <button key={g} onClick={() => setGenre(g)}>{g}</button>
+        ))}
+        <button onClick={() => setGenre('all genres')}>all genres</button>
+      </div>
     </div>
   )
 }
