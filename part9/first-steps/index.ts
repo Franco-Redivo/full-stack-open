608e116d6a957a681259d670aa726bee1610cae5
Franco Redivo <franco161101@gmail.com>
2026-02-19 15:02:53 -0300

    feat: add exercises endpoint for daily exercise calculation

--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 const app = express();
 
 app.get('/hello',(_req, res) => {
@@ -19,7 +20,24 @@ app.get('/bmi', (req, res) => {
 
     const bmi = calculateBmi(Number(height), Number(weight));
     return res.json({ weight, height, bmi });
-})
+});
+
+app.get('/exercises', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target} = req.body;
+
+    if(!daily_exercises || !target){
+        return res.status(400).send({error: 'Parameters missing'});
+    }
+    if(!(daily_exercises instanceof Array) || isNaN(Number(target))){
+        return res.status(400).send({error: 'Malformatted parameters'});
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculateExercises(daily_exercises, Number(target));
+
+    return res.send({result});
+});
 
 const PORT = 3003;
 
