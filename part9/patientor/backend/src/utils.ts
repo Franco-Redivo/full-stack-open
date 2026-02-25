6277fb8b8f7a7f383202debfbbfcaf0145dbf998
Franco-Redivo <franco161101@gmail.com>
2026-02-25 20:09:10 -0300

    feat: add utility functions for patient entry validation and parsing

---
+++ part9/patientor/backend/src/utils.ts
@@ -0,0 +1,72 @@
+import { NewPatientEntry, Gender } from "./types";
+
+
+//type guards
+const isString = (text: unknown): text is string => {
+    return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
+};
+
+const isGender = (gender: string): gender is Gender => {
+    return Object.values(Gender).map(v => v.toString()).includes(gender);
+};
+
+const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+    if(!object || typeof object !== 'object'){
+        throw new Error("Incorrect or missing data");
+    }
+
+    if('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object){
+        const newEntry: NewPatientEntry = {
+            name: parseName(object.name),
+            dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+            ssn: parseSsn(object.ssn),
+            gender: parseGender(object.gender),
+            occupation: parseOccupation(object.occupation)
+        };
+
+        return newEntry;
+    }
+
+    throw new Error('Incorrect data: some fields are missing');
+};
+
+const parseGender = (gender: unknown): Gender => {
+    if(!isString(gender) || !isGender(gender)){
+        throw new Error('Incorrect gender: ' + gender);
+    }
+    return gender;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+    if(!isString(occupation)){
+        throw new Error('Incorrect occupation');
+    }
+    return occupation;
+};
+
+const parseName = (name: unknown): string => {
+    if(!name || !isString(name)) {
+        throw new Error('Incorrect name');
+    }
+    return name;
+};
+
+const parseSsn = (ssn: unknown): string => {
+    if(!isString(ssn)){
+        throw new Error('Incorrect social security number');
+    }
+    return ssn;
+};
+
+const parseDateOfBirth = (date: unknown): string => {
+    if(!isString(date) || !isDate(date)) {
+        throw new Error('Incorrect date: ' + date);
+    }
+    return date;
+};
+
+export default toNewPatientEntry;
